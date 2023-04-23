bcef47f26696c165d871ed8e50e76aac61f8ebb6
masakarabala <48476935+masakarabala@users.noreply.github.com>
2023-04-23 21:17:18 +0200

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
 
 // Write your code here
+const num1=31;
+const num2=2;
+const multiply= num1*num2;
+console.log(multiply);
+
+
+
+// to generate a random integer between 1 and 10:
+Math.floor(Math.random() * 10) + 1;
+console.log(Math.floor(Math.random() * 10) + 1);
+
+/*Create a variable called `multiply` set to an equation that will multiply the
+variables `num1` and `num2`; the result of the multiplication should be `62`.
+2. Create a variable called `random` that will generate a random integer greater
+than 0.
+3. Create a variable called `mod` set to an equation that will calculate the
+remainder of dividing variable `num3` by `num4`; the remainder should be `4`.
+4. Create a variable called `max` that finds the highest number in a set; the
+value returned should be `20`.*/
+
+let num3=22;
+let num4=6;
+const mod= num3%num4;
+console.log(mod);
+
+/*
+working
+const x = [2 ,4,18,17,5,20];
+const max= Math.max();
+console.log(Math.max(...x));*/
+//working
+//console.log(Math.max(6, 7,20));
+const max= Math.max(6, 7,20);
+console.log(max);
